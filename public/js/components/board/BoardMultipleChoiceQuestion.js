d2c8d3644a4b5bedf199bbf48577e4c4f1b0bde2
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2015-06-07 20:39:20 +0200

    Show percentages on board for multiple choice question

--- public/js/components/board/BoardMultipleChoiceQuestion.js
+++ public/js/components/board/BoardMultipleChoiceQuestion.js
@@ -1,5 +1,11 @@
 var BoardMultipleChoiceQuestion = React.createClass({
     render: function() {
+        var totalAnswers = 0;
+
+        for (var answer in this.props.givenAnswers) {
+            totalAnswers += this.props.givenAnswers[answer].length;
+        }
+
         return (
             <div>
                 <header className="well">
@@ -16,8 +22,10 @@ var BoardMultipleChoiceQuestion = React.createClass({
                 </header>
                 <ul>
                     {this.props.answers.map((answer, i) => {
+                        var percentage = this.props.givenAnswers[i].length / totalAnswers * 100 || 0;
+
                         return (
-                            <li key={i}>{answer.label + " " + this.props.givenAnswers[i].length}</li>
+                            <li key={i}>{answer.label + " " + percentage}</li>
                         );
                     })}
                 </ul>
