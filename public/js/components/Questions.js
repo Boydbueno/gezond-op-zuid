e028cafeb8e141ea69ad2fa9ef4c6dd2308f2f34
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2015-06-08 23:28:29 +0200

    Added all multiple choice questions

--- public/js/components/Questions.js
+++ public/js/components/Questions.js
@@ -34,6 +34,152 @@ var Questions = {
                 "label": "12980"
             }
         ]
+    },
+    "3": {
+        "question": "Als je net zo’n goede voetballer wil worden als Robin van Persie, wat moet je dan als broodbeleg eten?",
+        "answers": [
+            {
+                "label": "Jam"
+            },
+            {
+                "label": "Kaas"
+            },
+            {
+                "label": "Pindakaas"
+            },
+            {
+                "label": "Kipfilet"
+            }
+        ]
+    },
+    "4": {
+        "question": "Welke vitaminesoort bevat pindakaas?",
+        "answers": [
+            {
+                "label": "Vitamine D: helpt bij de opbouw van botten en het gebit"
+            },
+            {
+                "label": "Vitamine A: voor een goede huid en ogen"
+            },
+            {
+                "label": "Vitamine E: het in stand houden van spier- en ander weefsel"
+            }
+        ]
+    },
+    "5": {
+        "question": "Welke soort producten zullen de meeste vitamines en mineralen bevatten?",
+        "answers": [
+            {
+                "label": "Normale producten"
+            },
+            {
+                "label": "Fairtrade producten"
+            },
+            {
+                "label": "Biologische producten"
+            }
+        ]
+    },
+    "6": {
+        "question": "Vitamine C is goed voor o.a. de weerstand en gezonde tanden. Waar is het nog meer goed voor?",
+        "answers": [
+            {
+                "label": "Je wordt minder snel oud"
+            },
+            {
+                "label": "Je groeit sneller"
+            },
+            {
+                "label": "Je spieren worden sterker"
+            }
+        ]
+    },
+    "7": {
+        "question": "Welke fruitsoorten bevatten de meeste vitamine C",
+        "answers": [
+            {
+                "label": "Banaan en sinaasappel"
+            },
+            {
+                "label": "Sinaasappel en kiwi"
+            },
+            {
+                "label": "Appel en peer"
+            }
+        ]
+    },
+    "8": {
+        "question": "Hoe merk je dat je te weinig vitamine A binnenkrijgt?",
+        "answers": [
+            {
+                "label": "Je wordt moe"
+            },
+            {
+                "label": "Je krijgt een drogere huid en dof haar"
+            },
+            {
+                "label": "Je hebt spierpijn"
+            }
+        ]
+    },
+    "9": {
+        "question": "Stelling: diepvriesgroenten bevatten evenveel vitamines als verse groenten",
+        "answers": [
+            {
+                "label": "Waar"
+            },
+            {
+                "label": "Niet waar"
+            }
+        ]
+    },
+    "10": {
+        "question": "Waarom is pure chocolade net iets gezonder dan witte chocolade?",
+        "answers": [
+            {
+                "label": "Van pure chocolade zit je sneller vol, waardoor je er minder van eet"
+            },
+            {
+                "label": "Pure chocolade bevat meer cacao. Cacao is goed voor je"
+            },
+            {
+                "label": "Pure chocolade is bitter in plaats van zoet. Zoete producten zijn altijd slechter voor je"
+            }
+        ]
+    },
+    "11": {
+        "question": "Vitamine B11, ook wel bekend als foliumzuur, is belangrijk voor het aanmaken van bloed. Waaruit haalt je lichaam vitamine B11?",
+        "answers": [
+            {
+                "label": "Tien uur slapen per nacht"
+            },
+            {
+                "label": "Het eten van groene groenten"
+            },
+            {
+                "label": "Bananen"
+            },
+            {
+                "label": "Veel lachen"
+            }
+        ]
+    },
+    "12": {
+        "question": "Hoe krijg net zo’n mooie glimlach als Beyoncé?",
+        "answers": [
+            {
+                "label": "Door veel uren buiten door te brengen"
+            },
+            {
+                "label": "Door het eten van wortels"
+            },
+            {
+                "label": "Je tanden te laten bleken"
+            },
+            {
+                "label": "Door veel water te drinken"
+            }
+        ]
     }
 };
 
