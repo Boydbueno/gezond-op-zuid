734e9860196dca85adfbbf85145f4d2356555be6
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2015-06-13 13:23:50 +0200

    Fixed styling of category overview

--- public/js/components/board/BoardFoodCategory.js
+++ public/js/components/board/BoardFoodCategory.js
@@ -9,17 +9,17 @@ var BoardFoodCategory = React.createClass({
                     <h1>Maandag 1 juni </h1>
                 </header>
                 <section>
-                    <image className="board-category-image" src="./img/icon_food.jpg" />
+                    <image className="board-category-image" src="./img/icon_food_active.jpg" />
                     <section className="board-subcategories">
                         <h1>Subcategorieën</h1>
-                        <ul>
+                        <ol>
                             <li><Link to="/board/food/1">Vitamines</Link></li>
                             <li></li>
                             <li></li>
                             <li></li>
                             <li></li>
                             <li></li>
-                        </ul>
+                        </ol>
                     </section>
                 </section>
             </div>
