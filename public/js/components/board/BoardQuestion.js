0a4a292eef69b8b2943490c32f158a6f44e20f91
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2015-06-22 23:42:51 +0200

    Removed console.log

--- public/js/components/board/BoardQuestion.js
+++ public/js/components/board/BoardQuestion.js
@@ -132,7 +132,7 @@ var BoardQuestion = React.createClass({
 
     nextQuestion: function() {
         var id = this.state.question.id + 1;
-        console.log(id);
+        
         if (id == 11) {
             this.transitionTo('/thanks');
             return;
